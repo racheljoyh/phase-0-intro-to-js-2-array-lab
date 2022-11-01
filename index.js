279958b6acb3f25a260143b57f266197ee77eb23
Rachel <racheljhumes@gmail.com>
2022-11-01 11:20:01 -0600

    commit tree 639829d9f5efc33f04aed3be6a48dd71274efb93

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat () {
+  cats = cats.push('Ralph');
+  return cats
+}
+
+function destructivelyPrependCat () {
+  cats = cats.unshift('Bob');
+  return cats;
+}
